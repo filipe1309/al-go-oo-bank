8cb5ea7aaab844d7596b4385869460e3d0f6333b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-07 18:04:01 -0300

    chore: multiple return

--- main.go
+++ main.go
@@ -9,21 +9,31 @@ type CurrentAccount struct {
 	balance    float64
 }
 
-func (c *CurrentAccount) withdraw(withdrawValue float64) string {
+func (c *CurrentAccount) Withdraw(withdrawValue float64) string {
 	canWithdraw := withdrawValue > 0 && withdrawValue <= c.balance
 	if canWithdraw {
 		c.balance -= withdrawValue
-		return "Success"
+		return "Withdaw success"
 	}
 	return "No balance"
+}
+
+func (c *CurrentAccount) Deposit(depositValue float64) (string, float64) {
+	if depositValue > 0 {
+		c.balance += depositValue
+		return "Deposit success", c.balance
+	}
+	return "Deposit value < 0", c.balance
 }
 
 func main() {
 	johnAccount := CurrentAccount{}
 	johnAccount.holder = "John"
 	johnAccount.balance = 500
-	fmt.Println(johnAccount.balance)
 
-	fmt.Println(johnAccount.withdraw(300))
+	fmt.Println(johnAccount.balance)
+	status, value := johnAccount.Deposit(2000)
+	fmt.Println(status, value)
+	fmt.Println(johnAccount.Withdraw(300))
 	fmt.Println(johnAccount.balance)
 }
