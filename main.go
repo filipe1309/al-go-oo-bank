0175e167466d873c932c44b74d440bdaefc012bb
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-06 21:46:07 -0300

    chore: var & types

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var holder string = "Filipe"
+	var numAgency int = 589
+	var numAccount int = 123465
+	var balance float64 = 125.50
+
+	fmt.Println(holder, numAgency, numAccount, balance)
+
+	var holder2 string = "Bob"
+	var numAgency2 int = 222
+	var numAccount2 int = 22222
+	var balance2 float64 = 222.22
+
+	fmt.Println(holder2, numAgency2, numAccount2, balance2)
+}
