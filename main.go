616ab4f508397f08d3f364bf63c3244d9309dfa9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-07 00:04:20 -0300

    chore: new & pointer

--- main.go
+++ main.go
@@ -13,4 +13,10 @@ func main() {
 	filipesAccount := CurrentAccount{holder: "Filipe", numAgency: 589, numAccount: 123456, balance: 125.50}
 	bobsAccount := CurrentAccount{"Bob", 222, 22222, 222.22}
 	fmt.Println(filipesAccount, bobsAccount)
+
+	var johnAccount *CurrentAccount
+	johnAccount = new(CurrentAccount)
+	johnAccount.holder = "John"
+	johnAccount.balance = 500
+	fmt.Println(*johnAccount)
 }
