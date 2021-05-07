7df9b50e4675addab2d00b721a17dabba162c2a0
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-06 21:52:40 -0300

    refactor: add struct

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import "fmt"
 
+type CurrentAccount struct {
+	holder     string
+	numAgency  int
+	numAccount int
+	balance    float64
+}
+
 func main() {
-	var holder string = "Filipe"
-	var numAgency int = 589
-	var numAccount int = 123465
-	var balance float64 = 125.50
+	fmt.Println(CurrentAccount{})
 
-	fmt.Println(holder, numAgency, numAccount, balance)
+	// "Filipe"
+	// 589
+	// 123465
+	// 125.50
 
-	var holder2 string = "Bob"
-	var numAgency2 int = 222
-	var numAccount2 int = 22222
-	var balance2 float64 = 222.22
+	// "Bob"
+	// 222
+	// 22222
+	// 222.22
 
-	fmt.Println(holder2, numAgency2, numAccount2, balance2)
 }
