abdeda6b037c403a0fff0c3497afaec4b0531c89
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-07 23:54:52 -0300

    refactor: change balance visibility

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"accounts"
-	"customers"
 	"fmt"
 )
 
 func main() {
-	johnCustomer := customers.Holder{"John", "132", "Dev"}
-	johnAccount := accounts.CurrentAccount{Holder: johnCustomer, NumAgency: 123, NumAccount: 123456, Balance: 100}
+	exAccount := accounts.CurrentAccount{}
+	exAccount.Deposit(100)
 
-	fmt.Println(johnAccount)
+	fmt.Println(exAccount.GetBalance())
 }
