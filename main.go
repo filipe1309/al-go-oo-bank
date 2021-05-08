0e8dc05aa2f32aa80e4b3215a8098326e417c5d9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-08 00:20:14 -0300

    feat: interface

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"fmt"
 )
 
+func PayBoleto(account verifyAccount, boletoValue float64) {
+	account.Withdraw(boletoValue)
+}
+
+type verifyAccount interface {
+	Withdraw(value float64) string
+}
+
 func main() {
 	johnAccount := accounts.SavingsAccount{}
 	johnAccount.Deposit(100)
-	johnAccount.Withdraw(555)
-
+	PayBoleto(&johnAccount, 60)
 	fmt.Println(johnAccount.GetBalance())
+
+	bobAccount := accounts.SavingsAccount{}
+	bobAccount.Deposit(500)
+	PayBoleto(&bobAccount, 10000)
+	fmt.Println(bobAccount.GetBalance())
+
 }
